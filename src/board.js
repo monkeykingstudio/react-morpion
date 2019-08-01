a205da0b7c8e686a6047c57b41ba54ec670f48ca
Raphael Vareilles <raphael.vareilles@gmail.com>
2019-08-01 08:22:16 +0200

    styles

--- src/board.js
+++ src/board.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import Header from './header.js';
 
-
 const Board = () => {
-    // return <InputEditor />;
     return (
         <div>
-            <Header />
-           
+            <Header />  
         </div>
     );
   };
-  
-//   const InputEditor = () => {
-//     const [greeting, setGreeting] = useState(
-//         'Welcome i can be edited !'
-//     );
-//     const handleChange = e => setGreeting(e.target.value);
-
-//     return (
-//         <div>
-//             <Header />
-//             <h1>{greeting}</h1>
-//             <input type='text' value={greeting} onChange={ handleChange }/>
-//         </div>
-//     );
-//   };
 
 export default Board;
